459f1d5a8eea7259db8b301dafadef7b0a7e180f
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2025-10-31 01:03:04 +0100

    feat: fix linter issues

--- internal/config/env.go
+++ internal/config/env.go
@@ -2,19 +2,16 @@ package config
 
 import (
 	"context"
-	"net/http"
-	"os"
 
 	"contactsAI/contacts/internal/db"
 
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/markbates/goth/gothic"
 )
 
 type Env struct {
 	*db.Queries
+
 	CookieStore cookie.Store
 }
 
@@ -31,23 +28,5 @@ func NewEnv(dbURL string) (*Env, error) {
 		return nil, pingErr
 	}
 
-	cookieStore := setupSessionStorage()
-	gothic.Store = cookieStore
-	return &Env{Queries: db.New(conn), CookieStore: cookieStore}, nil
-}
-
-func setupSessionStorage() cookie.Store {
-	sessionSecret, sessionKey := []byte(os.Getenv("SESSION_SECRET")), []byte(os.Getenv("SESSION_KEY"))
-
-	cookieStore := cookie.NewStore(sessionSecret, sessionKey)
-
-	cookieStore.Options(sessions.Options{
-		Path:     "/",
-		MaxAge:   86400 * 7, // 7 days
-		HttpOnly: true,      // Prevent XSS attacks
-		Secure:   true,      // Use only with HTTPS in production
-		SameSite: http.SameSiteLaxMode,
-	})
-
-	return cookieStore
+	return &Env{Queries: db.New(conn)}, nil
 }
--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -7,13 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const maxCorsAge = 12
+
 func setupCORS(router *gin.Engine) {
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:5173"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
 	config.AllowCredentials = true
-	config.MaxAge = 12 * time.Hour
+	config.MaxAge = maxCorsAge * time.Hour
 
 	router.Use(cors.New(config))
 }
